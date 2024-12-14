dee59187cceab6404f9f3dbfd5be56172c98d946
sheilla mungai <mungaisheilla19@gmail.com>
2024-12-14 12:41:23 +0300

    first commit

---
+++ es6practise/es6.js
@@ -0,0 +1,12 @@
+const add=()=>{
+    console.log("hello")
+}
+add()
+ //const name="alice"
+ //const last="jane"
+ //const message="hello"
+ //console.log (message)
+ const colors=['green','white','black']
+ console.log(colors[1])
+ const[first,second]=colors;
+ console.log(first)
---
+++ intro-to-html/index.js
@@ -0,0 +1,9 @@
+ function show_fun(){
+    if (weather ==='rainy'){
+        console.log('bring an umbrella')
+    }
+    else{
+        console.log('wear glasses')
+    }
+ }
+ show_fun()
